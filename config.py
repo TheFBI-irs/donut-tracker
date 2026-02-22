4463db8f7c0bd8e01a310208990083003afbe193
TheFBI-irs <federalbureauofinvestigationap@gmail.com>
2026-02-21 21:46:40 -0800

    retry add .gitignore

--- config.py
+++ config.py
@@ -12,9 +12,6 @@ WATCH_ITEMS = [
     "enchanted_golden_apple"
 ]
 
-# Discord webhook URL
-DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1474894959604793469/w5UvbaICRMnKuyw97VXKI6KrYPYZ7KhNf5hexkXWEJtMDdgYeEGiFxw21OhpyowhGxZu"
-
 # Alert thresholds
 PRICE_CHANGE_THRESHOLD = 0.05   # 5%
 VOLATILITY_THRESHOLD = 0.12     # 12%
