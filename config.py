6991fc72da3bdcfde7ac84ec60ee43d516eb83e0
TheFBI-irs <federalbureauofinvestigationap@gmail.com>
2026-02-21 21:53:39 -0800

    config-fix

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-# config.py
+import os
+from dotenv import load_dotenv
 
-# Donut Auction API endpoint
+load_dotenv()  # works locally, harmless on Railway
+
 API_URL = "https://api.donut.auction/orders"
 
-# Items we want to track
 WATCH_ITEMS = [
     "elytra",
     "netherite_ingot",
@@ -12,15 +13,6 @@ WATCH_ITEMS = [
     "enchanted_golden_apple"
 ]
 
-# Alert thresholds
-PRICE_CHANGE_THRESHOLD = 0.05   # 5%
-VOLATILITY_THRESHOLD = 0.12     # 12%
-CHECK_INTERVAL = 1800           # 30 minutes (API limit)
+DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
-FAIR_VALUES = {
-    "elytra": 280000000,
-    "netherite_ingot": 4300000,
-    "netherite_block": 38000000,
-    "dragon_head": 26000000,
-    "enchanted_golden_apple": 620000,
-}
+CHECK_INTERVAL = 1800
